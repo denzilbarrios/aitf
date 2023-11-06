e396707893ba27295b9b116d4feed32faacda238
Denzil Barrios <mcdbreg@gmail.com>
2023-11-06 11:36:51 +0000

    copia de rutina

--- angular/src/app/feature/user/home/home.component.ts
+++ angular/src/app/feature/user/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  imageUrl: string;
+  constructor( private http: HttpClient) { }
 
   ngOnInit(): void {
   }
-
+  getAndDisplayImage() {
+    // Realiza una solicitud HTTP GET para obtener la imagen del servidor
+    this.http.get('http://localhost:5000/apipy/reporte-predicciones-ingresos', { responseType: 'text' }).subscribe(
+      (data: string) => {
+        // Asigna la URL de la imagen a la variable imageUrl
+        this.imageUrl = data;
+      },
+      (error) => {
+        console.error('Error al obtener la imagen', error);
+      }
+    );
+  }
 }
